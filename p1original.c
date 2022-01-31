ff9a80768915930954dceac037433e508432d690
Bhuvanc2003 <93111679+Bhuvanc2003@users.noreply.github.com>
2022-01-31 06:05:13 +0000

    1+2=3

--- p1original.c
+++ p1original.c
@@ -1,2 +1,29 @@
+#include<stdio.h>
+int input()
+{
+  int a;
+  printf("enter a value : ");
+  scanf("%d",&a);
+  return a;
+  }
 
-}
+  int add(int a, int b)
+  {
+    int sum;
+    sum=a+b;
+    return sum;
+  }
+  void output(int a,int b,int sum)
+  {
+    printf("The sum of %d and %d = %d", a,b,sum);
+  }
+  int main()
+  {
+    int a,b,sum;
+    a = input();
+    b = input();
+    sum = add(a,b);
+    output(a,b,sum);
+    return 0;
+  }
+  
